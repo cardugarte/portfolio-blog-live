2d4284005bb16ab29175f74a49cd8966c9cceb78
Carlos Dugarte <soycarlosdugarte@gmail.com>
2023-05-20 00:17:47 -0300

    Se integra netlify cms

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,4 +1,10 @@
 import '../sass/main.scss';
+// import CMS from 'netlify-cms-app'
+
+// // Initialize the CMS object
+// CMS.init()
+// // Now the registry is available via the CMS object.
+// CMS.registerPreviewTemplate('my-template', MyTemplate)
 
 // This default export is required in a new `pages/_app.js` file.
 export default function MyApp({ Component, pageProps }) {
